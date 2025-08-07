0390434ec4d68915ed3b4896e886711f0956e064
Hevar <88142613+RageOfKurd@users.noreply.github.com>
2025-08-07 14:35:15 +0300

    done

--- src/app/projects/layout.jsx
+++ src/app/projects/layout.jsx
@@ -1,4 +1,4 @@
 // app/projects/layout.tsx
 export default function ProjectsLayout({ children }) {
-  return <div className={`bg-purple-800 min-h-screen`}>{children}</div>;
+  return <div className={`bg-white min-h-screen`}>{children}</div>;
 }
